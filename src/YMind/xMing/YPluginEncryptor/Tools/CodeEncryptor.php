0842056ad2c52b8a27df072c42ee8a1f487fa8f1
xMing <526817087@qq.com>
2018-01-10 01:22:51 +0800

    repair some BUG

--- src/YMind/xMing/YPluginEncryptor/Tools/CodeEncryptor.php
+++ src/YMind/xMing/YPluginEncryptor/Tools/CodeEncryptor.php
@@ -1,17 +1,19 @@
 <?php
 namespace  YMind\xMing\YPluginEncryptor\Tools;
 
-use YMind\xMing\YPluginEncryptor\Tools\Ways\way1;
+use YMind\xMing\YPluginEncryptor\Tools\Ways;
 
 class CodeEncryptor{
-     private function hasYml($dir){
+   private $main;
+   private $dirs=array();
+   private function hasYml($dir){
    $v=file_exists($dir."plugin.yml") ? true : false;
    return $v;
    }
   private function Run($id,$file){
          switch($id){
             case "1":
-            way1($file);
+            Ways::run("1",$file);
             }
        }
   private function getPHPfiles(){
@@ -19,26 +21,37 @@ class CodeEncryptor{
         foreach($this->dirs as $ds){
          foreach(glob($ds."*.php") as $ps){
          array_push($pfs,$ps);
+         $this->main->getLogger()->info("Getting file $ps");
           }
         }
       return $pfs;
       }
    private function saveDirs($url){
-      $this->dirs=array();
+      $this->main->getLogger()->info("sd1");
     foreach(glob($url."*") as $g){
+      $this->main->getLogger()->info("sd2");
        if(is_dir($g)){
+         $this->main->getLogger()->info("sd3");
+         $g=$g."/";
        array_push($this->dirs,$g);
-      $this->getDirs($g);
+      $this->main->getLogger()->info($g);
+      $this->saveDirs($g);
+      $this->main->getLogger()->info("Getting folder $g");
              }
             }
            }
-  public static function EncryptPlugin($dir){
+  public static function EncryptPlugin($dir,$main){
    $self= new CodeEncryptor();
+   $self->main=$main;
          if($self->hasYml($dir)){
+            $self->main->getLogger()->info("saved");
               $self->saveDirs($dir);
+            $self->main->getLogger()->info("getf");
               $pfs=$self->getPHPfiles();
+            $self->main->getLogger()->info("forrun");
               foreach($pfs as $s){
-                $self->Run("1",$s);            
+                $self->Run("1",$s);
+                $self->main->getLogger()->info("Encryptored $s");
               }
             unset($self);
             return "000";   
@@ -46,6 +59,7 @@ class CodeEncryptor{
             unset($self);
             return "202";
             }
+            unset($this->dirs);
              }
            }
   
--- src/YMind/xMing/YPluginEncryptor/Tools/CommandClass.php
+++ src/YMind/xMing/YPluginEncryptor/Tools/CommandClass.php
@@ -9,7 +9,7 @@ class CommandClass{
 	    	if($command=="ympe"){
 			if(isset($args[0])){
 				if(is_dir($main->getDataFolder().$args[0])){
-					$ce=CodeEncryptor::EncryptPlugin($main->getDataFolder().$args[0]);
+					$ce=CodeEncryptor::EncryptPlugin($main->getDataFolder().$args[0]."/",$main);
 					if($ce==="000"){
 						$sender->sendMessage("Sourse code of Plugin $args[0] has been encrypted.");
 						}else{						
@@ -19,7 +19,10 @@ class CommandClass{
 						     $sender->sendMessage("Unknow ERROR");
 						  break;
 						  case "202":
-	 					     $sender->sendMessage("Folder $args[0] is not a Plugin of PocketMine Server"); 
+	 					     $sender->sendMessage("Folder $args[0] is not a Plugin of PocketMine Server");
+                      break;
+                     default:
+                         $sender->sendMessage("233");
 						  }
 						}
 					}else{
